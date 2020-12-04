2e4126b79c2618d4da9eee751b561ba721425823
Dpirvoi <ragos.pirvoiu@ivfuture.uk>
2020-12-04 22:59:00 +0200

    2

---
+++ SizeOfGcc.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "Variabila int are "<<sizeof(int)<<" octeti\n";
+    cout << "Variabila float are "<<sizeof(float)<<" octeti\n";
+    cout <<"Variabila double are "<<sizeof(double)<<" octeti\n";
+    cout<<"Variabila unsigned are "<<sizeof(unsigned)<<" oceti\n";
+    cout<<"Variabila long are "<<sizeof(long)<<" octeti\n";
+    cout<<"Variabila char are "<<sizeof(char)<<" octeti\n";
+    cout<<"Variabila unsigned are "<<sizeof(unsigned)<<" octeti\n";
+}
